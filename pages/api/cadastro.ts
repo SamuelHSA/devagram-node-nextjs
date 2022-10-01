69d89a376501f4d190f78d21ecdf7b40b3677b5c
SamuelHSA <111927634+SamuelHSA@users.noreply.github.com>
2022-10-01 07:35:38 -0300

    Banco de dados Devagram

---
+++ pages/api/cadastro.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse} from 'next';
+import type { respostaPadraoMsg } from '../../types/respostasPadraoMsg';
+import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+
+
+import md5 from 'md5';
+const endpointCadastro = 
+    async(req : NextApiRequest, res : NextApiResponse<respostaPadraoMsg>) => {
+
+    if(req.method === 'POST'){
+        const usuario = req.body as CadastroRequisicao;
+
+        if(!usuario.nome || usuario.nome.length < 2){
+            return res.status(400).json({erro : 'Nome inválido'});
+        }
+
+        if(!usuario.email || usuario.email.length < 5
+            || !usuario.email.includes('@')
+            || !usuario.email.includes('.')){
+            return res.status(400).json({erro : 'Email inválido'});
+        }
+
+        if(!usuario.senha || usuario.senha.length < 4){
+            return res.status(400).json({erro : 'Senha inválida'});
+        }
+    
+
+        // validacao se ja existe usuario com o mesmo email
+        const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
+        if(usuariosComMesmoEmail && usuariosComMesmoEmail.length >0){
+            return res.status(400).json({erro : 'Já existe uma conta com o email informado'});
+
+        }
+
+        // salvar no banco de dados
+        const usuarioAsersalvo = {
+            nome : usuario.nome,
+            email : usuario.email,
+            senha : md5(usuario.senha)
+        }
+        await UsuarioModel.create(usuarioAsersalvo);
+        return res.status(200).json({msg : 'Usuário criado com sucesso'});
+    }
+    return res.status(405).json({erro : 'Método informado não é válido'});
+}
+
+export default conectarMongoDB(endpointCadastro);
